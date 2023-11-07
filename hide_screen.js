f82063c5f2f4b3d0f6c02aa6f9917f541a2b9519
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-07 14:11:33 -0500

    single file js function

--- hide_screen.js
+++ hide_screen.js
@@ -14,7 +14,6 @@ screenselect.addEventListener("change",function(){
         screen4.style.display="none";
         screenstyles.href="team8.css";
         screenscript.src="into8.js";
-        listofids=['round2first','round2second','round2third','round2fourth','round2fifth','round2sixth','round2seventh','round2eighth'];
     }else if(screenselect.value==="screen2"){
         screen1.style.display="non"
         screen2.style.display="block";
@@ -22,7 +21,6 @@ screenselect.addEventListener("change",function(){
         screen4.style.display="none";
         screenstyles.href="team16.css";
         screenscript.src="into16.js";
-        listofids=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth'];
     }else if(screenselect.value==="screen3"){
         screen1.style.display="none";
         screen2.style.display="none";
@@ -30,7 +28,6 @@ screenselect.addEventListener("change",function(){
         screen4.style.display="none";
         screenstyles.href="team32.css";
         screenscript.src="into32.js";
-        listofids=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth','twentieth','twenty1','twenty2','twenty3','twenty4','twenty5','twenty6','twenty7','twenty8','twenty9','thirty0','thirty1','thirty2'];
     }else if(screenselect.value==="screen4"){
         screen1.style.display="none";
         screen2.style.display="none";
@@ -38,6 +35,5 @@ screenselect.addEventListener("change",function(){
         screen4.style.display="block";
         screenstyles.href="team64.css";
         screenscript.src="into64.js";
-        listofids=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth','twentieth','twenty1','twenty2','twenty3','twenty4','twenty5','twenty6','twenty7','twenty8','twenty9','thirty0','thirty1','thirty2','thirty3','thirty4','thirty5','thirty6','thirty7','thirty8','thirty9','forty0','forty1','forty2','forty3','forty4','forty5','forty6','forty7','forty8','forty9','fifty0','fifty1','fifty2','fifty3','fifty4','fifty5','fifty6','fifty7','fifty8','fifty9','sixty0','sixty1','sixty2','sixty3','sixty4'];
     }
 });
