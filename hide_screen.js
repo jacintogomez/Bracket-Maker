0602e62ab33cb6515ada34fa78534f92b6f0d2d7
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-06 19:22:20 -0500

    hide screen operates for 4 screens in all4 file

--- hide_screen.js
+++ hide_screen.js
@@ -1,13 +1,29 @@
 const screenSelector = document.getElementById("screenSelector");
-const screen1 = document.getElementById("screen1");
-const screen2 = document.getElementById("screen2");
+const screen1 = document.getElementById("screen1"); //8 teams
+const screen2 = document.getElementById("screen2"); //16 teams
+const screen3 = document.getElementById("screen3"); //32 teams
+const screen4 = document.getElementById("screen4"); //64 teams
 
 screenSelector.addEventListener("change", function () {
     if (screenSelector.value === "screen1") {
         screen1.style.display = "block";
         screen2.style.display = "none";
+        screen3.style.display = "none";
+        screen4.style.display = "none";
     } else if (screenSelector.value === "screen2") {
         screen1.style.display = "none";
         screen2.style.display = "block";
+        screen3.style.display = "none";
+        screen4.style.display = "none";
+    } else if (screenSelector.value === "screen3") {
+        screen1.style.display = "none";
+        screen2.style.display = "none";
+        screen3.style.display = "block";
+        screen4.style.display = "none";
+    } else if (screenSelector.value === "screen4") {
+        screen1.style.display = "none";
+        screen2.style.display = "none";
+        screen3.style.display = "none";
+        screen4.style.display = "block";
     }
 });
