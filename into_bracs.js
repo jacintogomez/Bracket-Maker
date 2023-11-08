f3008c48a9960dfd7ec71a0c80a57f614310d742
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-11-07 22:46:14 -0500

    trim whitespace off of input

--- into_bracs.js
+++ into_bracs.js
@@ -3,7 +3,7 @@ let enterfield;
 
 function winner(me,you){
   me.style.background='yellow';
-  var finale=me.getElementsByClassName('namefield')[0].innerHTML;
+  let finale=me.getElementsByClassName('namefield')[0].innerHTML;
   you.innerHTML='Winner is '+finale+'!';
 }
 function colc(element,sister,next){
@@ -15,27 +15,27 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random()*(max-min+1)+min)
 }
 function sortintobracket(input,randomize){
-  var count=0;
+  let count=0;
   if(randomize===0){
-    for(var x of listofids){
+    for(let x of listofids){
       if(input[count]!=null){
         document.getElementById(x).getElementsByClassName('namefield')[0].innerHTML=input[count];
         count++;
       }
     }
   }else{
-    for(var x of listofids){
-      var i=randomIntFromInterval(0,input.length-1);
+    for(let x of listofids){
+      let i=randomIntFromInterval(0,input.length-1);
       document.getElementById(x).getElementsByClassName('namefield')[0].innerHTML=input[i];
       input.splice(i,1);
     }
   }
 }
 function convert(thing,randomize){
-  var result=[];
-  var temp='';
-  for(var i=0;i<thing.length;i++){
-    if(thing[i]!=' '){
+  let result=[];
+  let temp='';
+  for(let i=0;i<thing.length;i++){
+    if(thing[i]!==' '){
       temp+=thing[i];
     }else{
       result.push(temp);
@@ -46,7 +46,7 @@ function convert(thing,randomize){
   return sortintobracket(result,randomize);
 }
 function enterinput(randomize){
-  var names;
-  names=document.getElementById(enterfield).value;
+  let names=document.getElementById(enterfield).value;
+  names=names.trim();
   convert(names,randomize);
 }
