be8b45e81f4cebe847e51e9abf82f18b52a4e61d
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2025-04-09 09:13:23 -0400

    save/load file

---
+++ save_load.js
@@ -0,0 +1,7 @@
+const screenselect=document.getElementById('screenselector');
+const screenstyles=document.getElementById('styles');
+const screen1=document.getElementById('screen1'); //8 teams
+const screen2=document.getElementById('screen2'); //16 teams
+const screen3=document.getElementById('screen3'); //32 teams
+const screen4=document.getElementById('screen4'); //64 teams
+
