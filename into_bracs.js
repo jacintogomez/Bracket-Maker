55c0094e5f0ea158446be59d730acdfed9e0c2e4
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2026-01-06 23:20:26 -0500

    Fix rank order

--- into_bracs.js
+++ into_bracs.js
@@ -42,21 +42,12 @@ function randomPlacement(input){
 
 function seededPlacement(input){
     let smo;
-    if(input.length==8){
-        smo=[1,8,4,5,3,6,2,7];
-    }else if(input.length===16){
-        smo=[1,16,8,9,5,12,4,13,3,14,6,11,7,10,2,15];
-    }else if(input.length===32){
-        smo=[1,32,16,17,8,25,9,24,5,28,12,21,4,29,13,20,3,30,14,19,6,27,11,22,7,26,10,23,2,31,15,18];
-    }else if(input.length===64){
-        smo=[1,64,32,33,16,49,17,48,8,57,25,40,9,56,24,41,5,60,28,37,12,53,21,44,4,61,29,36,13,52,20,45,3,62,30,35,14,51,19,46,6,59,27,38,11,54,22,43,7,58,26,39,10,55,23,42,2,63,31,34,15,50,18,47];
-    }
-    let count=0;
-    for(let x of listofids){
-        if(input[smo[count]]!=null){
-            document.getElementById(x).getElementsByClassName('namefield')[0].innerHTML=input[smo[count]];
-            count++;
-        }
+    if(input.length===8){smo=[1,8,4,5,3,6,2,7];}
+    else if(input.length===16){smo=[1,16,8,9,5,12,4,13,3,14,6,11,7,10,2,15];}
+    else if(input.length===32){smo=[1,32,16,17,8,25,9,24,5,28,12,21,4,29,13,20,3,30,14,19,6,27,11,22,7,26,10,23,2,31,15,18];}
+    else if(input.length===64){smo=[1,64,32,33,16,49,17,48,8,57,25,40,9,56,24,41,5,60,28,37,12,53,21,44,4,61,29,36,13,52,20,45,3,62,30,35,14,51,19,46,6,59,27,38,11,54,22,43,7,58,26,39,10,55,23,42,2,63,31,34,15,50,18,47];}
+    for(let i=0;i<input.length;i++){
+        document.getElementById(listofids[i]).getElementsByClassName('namefield')[0].innerHTML=input[smo[i]-1];
     }
 }
 
